326ea5b42dea3ca813ba903fbc2acaac2c142f65
harmik007 <harmik.ecommerce@gmail.com>
2026-05-14 16:36:56 +0530

    after upload in vercel

--- config/db.js
+++ config/db.js
@@ -1,19 +1,27 @@
-const mysql = require('mysql2');
+// const mysql = require('mysql2');
 
-const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+// const connection = mysql.createConnection({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+// });
+
+// connection.connect((err) => {
+//   if (err) {
+//     console.log('Database connection failed');
+//     return;
+//   }
+
+//   console.log('Database Connected');
+// });
+
+// module.exports = connection;
+
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  connectionString: process.env.POSTGRES_URL,
 });
 
-connection.connect((err) => {
-  if (err) {
-    console.log('Database connection failed');
-    return;
-  }
-
-  console.log('Database Connected');
-});
-
-module.exports = connection;
+module.exports = pool;
--- routes/formRoutes.js
+++ routes/formRoutes.js
@@ -23,30 +23,62 @@
 // module.exports = router;
 
 
+// const express = require('express');
+// const router = express.Router();
+// const db = require('../config/db');
+
+// router.get('/submit', (req, res) => {
+//   res.send('API is working. Use POST request to submit form.');
+// });
+
+// router.post('/submit', (req, res) => {
+//   const { name, email } = req.body;
+
+//   const sql = 'INSERT INTO users (name, email) VALUES (?, ?)';
+
+//   db.query(sql, [name, email], (err, result) => {
+//     if (err) {
+//       return res.status(500).json({
+//         message: 'Database Error',
+//       });
+//     }
+
+//     res.json({
+//       message: 'Form Submitted Successfully',
+//     });
+//   });
+// });
+
+// module.exports = router;
+
 const express = require('express');
 const router = express.Router();
 const db = require('../config/db');
 
 router.get('/submit', (req, res) => {
-  res.send('API is working. Use POST request to submit form.');
+  res.send('API is working');
 });
 
-router.post('/submit', (req, res) => {
-  const { name, email } = req.body;
+router.post('/submit', async (req, res) => {
+  try {
+    const { name, email } = req.body;
 
-  const sql = 'INSERT INTO users (name, email) VALUES (?, ?)';
-
-  db.query(sql, [name, email], (err, result) => {
-    if (err) {
-      return res.status(500).json({
-        message: 'Database Error',
-      });
-    }
+    await db.query(
+      'INSERT INTO users(name, email) VALUES($1, $2)',
+      [name, email]
+    );
 
     res.json({
       message: 'Form Submitted Successfully',
     });
-  });
+
+  } catch (error) {
+    console.log(error);
+
+    res.status(500).json({
+      message: 'Database Error',
+    });
+  }
 });
 
 module.exports = router;
